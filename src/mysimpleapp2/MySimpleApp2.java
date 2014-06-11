66c45053a012e33ef21f0c30f379314b50df9e97
eeblove53@hotmail.com <>
2014-06-11 10:22:36 +0700

    123

---
+++ src/mysimpleapp2/MySimpleApp2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mysimpleapp2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MySimpleApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+    
+}
